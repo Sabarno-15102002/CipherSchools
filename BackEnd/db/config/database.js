8fd48dea3ba03c9b98028a0318c5c4e219c9f937
Sabarno Biswas <sabarnobiswas2002@gmail.com>
2023-04-20 04:20:09 +0530

    Update database.js

--- BackEnd/db/config/database.js
+++ BackEnd/db/config/database.js
@@ -3,5 +3,4 @@ const { DB_USERNAME, DB_PASSWORD, DB_CLUSTER } = require("./config");
 
 module.exports = {
     MongoURI: `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_CLUSTER}.yfwfi0n.mongodb.net/CipherschoolsDB?retryWrites=true&w=majority`
-    // MongoURI: `mongodb+srv://admin-sabarno-ananya:learnwithpix123@cluster0.yfwfi0n.mongodb.net/CipherschoolsDB?retryWrites=true&w=majority`
 }
