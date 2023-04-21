0cbb565478452e15cea535a2deb6dfecbf3d82a2
Sabarno-15102002 <sabarnobiswas2002@gmail.com>
2023-04-21 18:43:26 +0530

    Follower Updated

---
+++ FrontEnd/src/Component/Follower.js
@@ -0,0 +1,134 @@
+const URL = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png";
+export default followersArr = [
+    {
+        "name": "Aarav Singh",
+        "job": "College Student",
+        "followers": 13,
+        "img": URL
+    },
+    {
+        "name": "Aryan Sharma",
+        "job": "Teaching",
+        "followers": 31,
+        "img": URL
+    },
+    {
+        "name": "Arnav Gupta",
+        "job": "Job",
+        "followers": 43,
+        "img": URL
+    },
+    {
+        "name": "Aditya Patel",
+        "job": "Schooling",
+        "followers": 7,
+        "img": URL
+    },
+    {
+        "name": "Ishaan Chakraborty",
+        "job": "Freelancing",
+        "followers": 27,
+        "img": URL
+    },
+    {
+        "name": "Ishita Singh",
+        "job": "College Student",
+        "followers": 19,
+        "img": URL
+    },
+    {
+        "name": "Jhanvi Desai",
+        "job": "Job",
+        "followers": 44,
+        "img": URL
+    },
+    {
+        "name": "Jatin Mehta",
+        "job": "Teaching",
+        "followers": 29,
+        "img": URL
+    },
+    {
+        "name": "Kavya Reddy",
+        "job": "Freelancing",
+        "followers": 35,
+        "img": URL
+    },
+    {
+        "name": "Kartik Sharma",
+        "job": "College Student",
+        "followers": 23,
+        "img": URL
+    },
+    {
+        "name": "Krishna Singh",
+        "job": "Schooling",
+        "followers": 8,
+        "img": URL
+    },
+    {
+        "name": "Kabir Verma",
+        "job": "Job",
+        "followers": 41,
+        "img": URL
+    },
+    {
+        "name": "Mihir Patel",
+        "job": "Freelancing",
+        "followers": 37,
+        "img": URL
+    },
+    {
+        "name": "Mira Shah",
+        "job": "College Student",
+        "followers": 14,
+        "img": URL
+    },
+    {
+        "name": "Maya Gupta",
+        "job": "Teaching",
+        "followers": 24,
+        "img": URL
+    },
+    {
+        "name": "Neha Singh",
+        "job": "Job",
+        "followers": 46,
+        "img": URL
+    },
+    {
+        "name": "Naman Sharma",
+        "job": "Freelancing",
+        "followers": 34,
+        "img": URL
+    },
+    {
+        "name": "Nidhi Chakraborty",
+        "job": "College Student",
+        "followers": 17,
+        "img": URL
+    },
+    {
+        "name": "Nishant Desai",
+        "job": "Schooling",
+        "followers": 6,
+        "img": URL
+    },
+    {
+        "name": "Prisha Mehta",
+        "job": "Teaching",
+        "followers": 26,
+        "img": URL
+    },
+    {
+        "name": "Parth Reddy",
+        "job": "Job",
+        "followers": 48,
+        "img": URL
+    },
+    {
+        "name": "Paras Sharma",
+        "job": "Freelancing",
+        "followers": 34,
+        "img": URL
+    }]
